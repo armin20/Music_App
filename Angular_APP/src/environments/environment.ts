576977f90da11300acce8884c6ef339358757fa3
Armin Sharifiyan <77298532+armin20@users.noreply.github.com>
2023-01-31 20:03:25 -0500

    Update environment.ts

--- Angular_APP/src/environments/environment.ts
+++ Angular_APP/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   clientID: "954aba3c1a9247659c512db291d84860",
   clientSecret: "88bf9fea118c48c68ce3edc4db9f1948",
-  userAPIBase: "https://calm-anchorage-61423.herokuapp.com/api/user"
+  userAPIBase: "http://ec2-3-86-18-152.compute-1.amazonaws.com:8080/api/user"
 };
 
 /*
